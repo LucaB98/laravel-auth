2bc3efc58056e6c92c9abdc15988ca45d5896097
LucaB98 <lucabellan98@gmail.com>
2024-03-19 21:22:46 +0100

    create route and scaffolding controller model and seed

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title =  fake()->text(20);
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'content' => fake()->paragraphs(15, true),
+            'image' => fake()->imageUrl(250, 250, true)
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
+use App\Http\Controllers\Admin\ProjectController;
+
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +20,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', GuestHomeController::class) ->name('guest.home');
 
-Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+
+Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function(){
+    
+    Route::get('', AdminHomeController::class)->name('home');
+    Route::resource('projects', ProjectController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
